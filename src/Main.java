00929bfe93e1d781fe4a6e62bbad758e5e52a10b
Tatyana Dmitrieva <visyar76@gmail.com>
2022-06-14 22:15:37 +0300

    Changed the algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         float balance = 100F; //остаток на счете
-        float added = 1100F; // пополнение счета
-        int bonus = (int)added / 100;
-        if (added <= 1000) {
-            bonus = 0;
+        float added = 100F; // пополнение счета
+        int bonus = 0;
+        float newBalance = 0;
+        if (added >= 1000) {
+            bonus = (int) added / 100;
+            newBalance = balance + added + bonus;
+            System.out.println("Вам начислено " + bonus + " бонусных рублей.");
+            System.out.println("На вашем балансе " + newBalance + " рублей.");
+        } else {
+            newBalance = balance + added;
+            System.out.println("На вашем балансе " + newBalance + " рублей.");
         }
-        float newBalance = (balance + added + bonus);
-        System.out.println("Вам начислено " + bonus + " бонусных рублей.");
-        System.out.println("На вашем балансе " + newBalance + " рублей.");
     }
 }
