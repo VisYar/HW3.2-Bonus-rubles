00c71f06340ff6fbd21e73941f1bd638ee5833fb
Tatyana Dmitrieva <visyar76@gmail.com>
2022-06-14 20:28:13 +0300

    Changed the type to float

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
-        int added = 1100;
-        int bonus = added / 100;
-        if (added < 1000) {
+        float balance = 100F; //остаток на счете
+        float added = 1100F; // пополнение счета
+        int bonus = (int)added / 100;
+        if (added <= 1000) {
             bonus = 0;
         }
-        int newBalance = (balance + added + bonus);
+        int newBalance = ((int)balance + (int)added + bonus);
         System.out.println("Вам начислено " + bonus + " бонусных рублей.");
         System.out.println("На вашем балансе " + newBalance + " рублей.");
     }
